fe866cedba6f63d3a91f590468d65d98badc560d
liaohui5 <secretx500@gmail.com>
2026-09-03 21:53:28 +0800

    feat: add task list collapse with Ctrl+\ shortcut

--- src/components/PlanLayout.vue
+++ src/components/PlanLayout.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted } from "vue";
+import { useLocalStorage } from "@vueuse/core";
 import CategoryList from "@/components/CategoryList.vue";
 import TaskList from "@/components/TaskList.vue";
 import TaskDetail from "@/components/TaskDetail.vue";
 import { usePlanManager } from "@/composables/usePlanManager";
 
 const { selectedTask, updateTaskContent } = usePlanManager();
+
+const taskListCollapsed = useLocalStorage("plan-manager-tasklist-collapsed", false);
+
+function toggleTaskList() {
+  taskListCollapsed.value = !taskListCollapsed.value;
+}
+
+function onKeydown(e: KeyboardEvent) {
+  if (e.ctrlKey && e.key === "\\") {
+    e.preventDefault();
+    toggleTaskList();
+  }
+}
+
+onMounted(() => window.addEventListener("keydown", onKeydown));
+onBeforeUnmount(() => window.removeEventListener("keydown", onKeydown));
 </script>
 
 <template>
@@ -12,8 +30,13 @@ const { selectedTask, updateTaskContent } = usePlanManager();
     <div class="w-64 border-r border-border bg-card shrink-0">
       <CategoryList />
     </div>
-    <div class="w-80 border-r border-border bg-card shrink-0">
-      <TaskList />
+    <div
+      class="shrink-0 overflow-hidden transition-[width] duration-200 ease-in-out"
+      :class="taskListCollapsed ? 'w-0' : 'w-80'"
+    >
+      <div class="w-80 h-full border-r border-border bg-card">
+        <TaskList @toggle="toggleTaskList" />
+      </div>
     </div>
     <div class="flex-1 bg-background min-w-0 h-full">
       <TaskDetail :task="selectedTask" @update:content="updateTaskContent" />
